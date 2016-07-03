6f119a2db646087fe7a06a65df2a8e7826dd590f
Matthew Glazar <strager.nds@gmail.com>
2016-07-03 10:19:58 -0700

    Use Mach semaphores instead of dispatch semaphores.

--- mvar-internal.h
+++ mvar-internal.h
@@ -1,29 +1,37 @@
 #ifndef MVAR_INTERNAL_H_2222195A_A36B_4D94_9066_E8645E3AA5D9
 #define MVAR_INTERNAL_H_2222195A_A36B_4D94_9066_E8645E3AA5D9
 
-#define MVAR_USE_DISPATCH_SEMAPHORE 1
+#define MVAR_USE_MACH_SEMAPHORE 1
 
 #include <mvar.h>
 
-#include <pthread.h>
 #include <stdatomic.h>
 #include <stdbool.h>
 
-#if MVAR_USE_DISPATCH_SEMAPHORE
-#include <dispatch/dispatch.h>
+#if MVAR_USE_MACH_SEMAPHORE
+#include <mach/mach_types.h>
+#include <stdint.h>
 #endif
 
 struct MVar {
 	void *_Atomic value;
-#if MVAR_USE_DISPATCH_SEMAPHORE
-        /* Signalled when mvar_put is unblocked. */
-	dispatch_semaphore_t put_semaphore;
-        /* Signalled when mvar_take is unblocked. */
-	dispatch_semaphore_t take_semaphore;
+#if MVAR_USE_MACH_SEMAPHORE
+        /* put_sem is signalled when mvar_put is unblocked. */
+        /* take_sem is signalled when mvar_take is unblocked. */
+        semaphore_t _Atomic put_sem;
+        semaphore_t _Atomic take_sem;
+        int32_t _Atomic put_sem_value;
+        int32_t _Atomic take_sem_value;
 #endif
 };
 
 bool mvar_init (MVar *mvar, void *value);
 void mvar_destroy (MVar *mvar);
+
+#if MVAR_USE_MACH_SEMAPHORE
+semaphore_t mvar_sem_init (semaphore_t _Atomic *sem);
+void mvar_sem_signal (semaphore_t _Atomic *sem, int32_t _Atomic *sem_value);
+void mvar_sem_wait (semaphore_t _Atomic *sem, int32_t _Atomic *sem_value);
+#endif
 
 #endif /* MVAR_INTERNAL_H_2222195A_A36B_4D94_9066_E8645E3AA5D9 */
--- mvar.c
+++ mvar.c
@@ -4,6 +4,13 @@
 #include <stdatomic.h>
 #include <stdlib.h>
 
+#if MVAR_USE_MACH_SEMAPHORE
+#include <mach/kern_return.h>
+#include <mach/mach_init.h>
+#include <mach/semaphore.h>
+#include <mach/task.h>
+#endif
+
 MVar *
 mvar_new (void *value)
 {
@@ -30,15 +37,13 @@ mvar_init (MVar *mvar, void *value)
 {
 	*mvar = (struct MVar) {
 		.value = value,
-#if MVAR_USE_DISPATCH_SEMAPHORE
-		.put_semaphore = dispatch_semaphore_create (value ? 0 : 1),
-		.take_semaphore = dispatch_semaphore_create (value ? 1 : 0),
+#if MVAR_USE_MACH_SEMAPHORE
+		.put_sem_value = 0,
+		.take_sem_value = 0,
+		.put_sem = SEMAPHORE_NULL,
+		.take_sem = SEMAPHORE_NULL,
 #endif
 	};
-#if MVAR_USE_DISPATCH_SEMAPHORE
-	assert (mvar->put_semaphore);
-	assert (mvar->take_semaphore);
-#endif
 	return true;
 }
 
@@ -46,8 +51,14 @@ void
 mvar_destroy (MVar *mvar)
 {
 	assert (!atomic_load_explicit (&mvar->value, memory_order_relaxed));
-	dispatch_release (mvar->put_semaphore);
-	dispatch_release (mvar->take_semaphore);
+	if (mvar->put_sem != SEMAPHORE_NULL) {
+		kern_return_t rc = semaphore_destroy (mach_task_self(), mvar->put_sem);
+		assert (rc == KERN_SUCCESS);
+	}
+	if (mvar->take_sem != SEMAPHORE_NULL) {
+		kern_return_t rc = semaphore_destroy (mach_task_self(), mvar->take_sem);
+		assert (rc == KERN_SUCCESS);
+	}
 }
 
 void
@@ -55,11 +66,11 @@ mvar_put (MVar *mvar, void *value)
 {
 	assert (value != NULL);
 	for (;;) {
-#if MVAR_USE_DISPATCH_SEMAPHORE
+#if MVAR_USE_MACH_SEMAPHORE
 		if (mvar_try_put (mvar, value)) {
 			break;
 		}
-		dispatch_semaphore_wait (mvar->put_semaphore, DISPATCH_TIME_FOREVER);
+		mvar_sem_wait (&mvar->put_sem, &mvar->put_sem_value);
 #else
 #error "Unknown implementation"
 #endif
@@ -72,12 +83,12 @@ mvar_take (MVar *mvar)
 	void *value;
 
 	for (;;) {
-#if MVAR_USE_DISPATCH_SEMAPHORE
+#if MVAR_USE_MACH_SEMAPHORE
 		value = mvar_try_take (mvar);
 		if (value) {
 			return value;
 		}
-		dispatch_semaphore_wait (mvar->take_semaphore, DISPATCH_TIME_FOREVER);
+		mvar_sem_wait (&mvar->take_sem, &mvar->take_sem_value);
 #else
 #error "Unknown implementation"
 #endif
@@ -92,7 +103,7 @@ mvar_try_put (MVar *mvar, void *value)
 	assert (value != NULL);
 	void *expected = NULL;
 	if (atomic_compare_exchange_strong (&mvar->value, &expected, value)) {
-		dispatch_semaphore_signal (mvar->take_semaphore);
+		mvar_sem_signal (&mvar->take_sem, &mvar->take_sem_value);
 		return true;
 	}
 	return false;
@@ -109,8 +120,60 @@ mvar_try_take (MVar *mvar)
 			return NULL;
 		}
 		if (atomic_compare_exchange_weak (&mvar->value, &old_value, NULL)) {
-			dispatch_semaphore_signal (mvar->put_semaphore);
+			mvar_sem_signal (&mvar->put_sem, &mvar->put_sem_value);
 			return old_value;
 		}
 	}
 }
+
+#if MVAR_USE_MACH_SEMAPHORE
+semaphore_t
+mvar_sem_init (semaphore_t _Atomic *sem)
+{
+	/* N.B. semaphore_t itself doesn't need to be synchronized. */
+	semaphore_t old_sem = atomic_load_explicit (sem, memory_order_relaxed);
+	if (old_sem != SEMAPHORE_NULL) {
+		return old_sem;
+	}
+
+	semaphore_t new_sem;
+	kern_return_t rc;
+	rc = semaphore_create (mach_task_self (), &new_sem, SYNC_POLICY_FIFO, 0);
+	assert (rc == KERN_SUCCESS);
+
+	if (!atomic_compare_exchange_strong (sem, &old_sem, new_sem)) {
+		assert (old_sem != SEMAPHORE_NULL);
+		rc = semaphore_destroy (mach_task_self (), new_sem);
+		assert (rc == KERN_SUCCESS);
+		return old_sem;
+	}
+	return new_sem;
+}
+
+void
+mvar_sem_signal (semaphore_t _Atomic *sem, int32_t _Atomic *sem_value)
+{
+	int32_t old_value = atomic_fetch_add (sem_value, 1);
+	if (old_value >= 0) {
+		return;
+	}
+	semaphore_t s = mvar_sem_init (sem);
+	kern_return_t rc = semaphore_signal (s);
+	assert (rc == KERN_SUCCESS);
+}
+
+void
+mvar_sem_wait (semaphore_t _Atomic *sem, int32_t _Atomic *sem_value)
+{
+	int32_t old_value = atomic_fetch_sub (sem_value, 1);
+	if (old_value > 0) {
+		return;
+	}
+	semaphore_t s = mvar_sem_init (sem);
+	kern_return_t rc;
+	do {
+		rc = semaphore_wait (s);
+		assert (rc == KERN_SUCCESS || rc == KERN_ABORTED);
+	} while (rc == KERN_ABORTED); /* EINTR */
+}
+#endif
